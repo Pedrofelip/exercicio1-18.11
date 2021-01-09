f9b211c71939364aede902c69601a0bec660d457
unknown <pedrofelipbarros@gmail.com>
2021-01-09 10:32:00 -0300

    exercicio1 aula 18.11 finalizado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+
+            Console.WriteLine("adicione a nota a baixo:");
+            nota = int.Parse(Console.ReadLine());
+
+                while (nota<0 || nota>10)
+                {
+                    Console.WriteLine("nota invalida, por favor tente novamente");
+                    Console.WriteLine("adicione a nota a baixo:");
+                    nota = int.Parse(Console.ReadLine());
+                }
+            Console.WriteLine("nota arquivada!");
+        }
+    }
+}
